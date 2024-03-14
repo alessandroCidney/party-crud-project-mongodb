455bb7f8da42a0444244df655c6655dd6b37e8c4
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2024-03-14 00:49:44 -0300

    feat: Configuring server.js

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+// modules
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+const bodyParser = require('body-parser')
+
+const cors = require('cors')
+
+// routes
+
+// middlewares
+
+// config
+
+const dbName = 'partytimeb'
+const port = 3000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+
+// mongodb connection
+
+mongoose.connect(
+  `mongodb://localhost/${dbName}`,
+  {
+    // useNewUrlParser: true,
+    // useFindAndModify: false,
+    // useUnifiedTopology: true,
+  },
+)
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Rota teste!'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`O backend está rodando na porta ${port}`)
+})
