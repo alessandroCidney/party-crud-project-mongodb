a26a7663d17860f070ca452c86f4317161d842b7
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2024-03-17 02:02:22 -0300

    feat: Created removal and update routes

--- backend/routes/partyRoutes.js
+++ backend/routes/partyRoutes.js
@@ -172,13 +172,100 @@ router.get('/:id', async (req, res) => {
         })
       } else {
         return res.status(400).json({
-          error: 'Acesso negado',
+          error: 'Acesso negado!',
         })
       }
     }
   } catch (err) {
     return res.status(400).json({
       error: 'Este evento não existe'
+    })
+  }
+})
+
+// delete a party
+router.delete('/', verifyToken, async (req, res) => {
+  const token = req.header('auth-token')
+  const user = await getUserByToken(token)
+  const partyId = req.body.id
+  const userId = user._id.toString()
+
+  try {
+    await Party.deleteOne({ _id: partyId, userId })
+    res.json({
+      error: null,
+      msg: 'Evento removido com sucesso!',
+    })
+  } catch (err) {
+    res.status(400).json({
+      error: 'Acesso negado!',
+    })
+  }
+})
+
+// update a party
+router.put('/', verifyToken, upload.fields([{ name: 'photos' }]), async (req, res) => {
+  // req body
+  const { title, description, party_date: partyDate, id: partyId, user_id: partyUserId, privacy } = req.body
+
+  let files = []
+
+  if (req.files) {
+    files = req.files.photos
+  }
+
+  // validations
+  if (title == 'null' || description == 'null' || partyDate == 'null') {
+    return res.status(400).json({
+      error: 'Preencha pelo menos nome, descrição e data.',
+    })
+  }
+
+  // verify user
+  const token = req.header('auth-token')
+
+  const userByToken = await getUserByToken(token)
+
+  const userId = userByToken._id.toString()
+
+  if (userId !== partyUserId) {
+    return res.status(400).json({
+      error: 'Acesso negado!',
+    })
+  }
+
+  // build party object
+  const party = {
+    id: partyId,
+    title,
+    description,
+    partyDate,
+    privacy,
+    userId,
+  }
+
+  let photos = []
+
+  if (files && files.length > 0) {
+    photos = files.map((photo) => photo.path)
+    party.photos = photos
+  }
+
+  try {
+    const updatedParty = await Party.findOneAndUpdate(
+      { _id: partyId, userId },
+      { $set: party },
+      { new: true },
+    )
+
+    return res.json({
+      error: null,
+      msg: 'Evento atualizado com sucesso!',
+      data: updatedParty,
+    })
+  } catch (err) {
+    return res.status(400).json({
+      error: err,
     })
   }
 })
